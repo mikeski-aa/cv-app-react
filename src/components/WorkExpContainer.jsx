5ebf2477326ccc2cd78c1d87f713b30e5a48031b
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-05-19 12:08:13 +0200

    Github issue resolved?

--- src/components/WorkExpContainer.jsx
+++ src/components/WorkExpContainer.jsx
@@ -24,12 +24,19 @@ function WorkExpContainer() {
   }
 
 // need to create if / case for to handle data coming from different input sources
-  function handleStateChange(input, source) {
-    const newData = {source: `${input}`}
-    setJobInfo({
-      ...jobInfo, 
-      ...newData})
-  }
+function handleStateChange(input, source) {
+  if (source === "companyInput") {
+    setJobInfo({...jobInfo, companyInput: input})
+  } else if (source === "dateFrom") {
+    setJobInfo({...jobInfo, dateFrom: input})
+  } else if (source === "dateUntil") {
+    setJobInfo({...jobInfo, dateUntil: input})
+  } else if (source === "jobTitle") {
+    setJobInfo({...jobInfo, jobTitle: input})
+  } else if (source === "jobOverview") {
+    setJobInfo({...jobInfo, jobOverview: input})
+  } 
+}
 
   return (
     <div className="workExp">
