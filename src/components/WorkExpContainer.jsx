62b1e34abf118c7d4ce34c22c3ef581d28b6ba34
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-05-20 12:30:49 +0200

    Deleting cards is not functional

--- src/components/WorkExpContainer.jsx
+++ src/components/WorkExpContainer.jsx
@@ -2,23 +2,68 @@ import { AddNewButton } from "./AddNewButton";
 import { WorkExpTemplate } from "./WorkExpTemplate";
 import { WorkExpModal } from "./WorkExpModal";
 import { useState } from "react";
-import { v4 as uuidv4} from 'uuid'
-
+import { v4 as uuidv4 } from "uuid";
 
 function WorkExpContainer() {
   const [modalVisible, setModalVisible] = useState(false);
-  const [tempJobInfo, setTempJobInfo] = useState([{
-    id: "",
-    companyInput: "",
-    dateFrom: "",
-    dateUntil: "",
-    jobTitle: "",
-    jobOverview: ""
+  const [tempJobInfo, setTempJobInfo] = useState([
+    {
+      id: "",
+      companyInput: "",
+      dateFrom: "",
+      dateUntil: "",
+      jobTitle: "",
+      jobOverview: "",
+    },
+  ]);
+  const [jobList, setJobList] = useState([]);
 
-  }]);
-  const [jobList, setJobList] = useState([])
+  // resets state - used when closing modal by saving or cancelling addition
+  function resetTempState() {
+    setTempJobInfo({
+      id: "",
+      companyInput: "",
+      dateFrom: "",
+      dateUntil: "",
+      jobTitle: "",
+      jobOverview: "",
+    });
+  }
 
+  // upon opening the modal, new id number is generated for the job, modal made visible
+  function handleOpenClick() {
+    setTempJobInfo({ id: uuidv4() });
+    setModalVisible(true);
+  }
 
+  // clicking cancel will reset the temporary state, closing modal
+  function handleCloseClick() {
+    setModalVisible(false);
+    resetTempState();
+  }
+
+  // on clicking save, a new div should be created within workExp div called WorkExperienceDetails
+  function handleSaveClick() {
+    setModalVisible(false);
+    setJobList([...jobList, tempJobInfo]);
+  }
+
+  // need to create if / case for to handle data coming from different input sources
+  function handleStateChange(input, source) {
+    if (source === "companyInput") {
+      setTempJobInfo({ ...tempJobInfo, companyInput: input });
+    } else if (source === "dateFrom") {
+      setTempJobInfo({ ...tempJobInfo, dateFrom: input });
+    } else if (source === "dateUntil") {
+      setTempJobInfo({ ...tempJobInfo, dateUntil: input });
+    } else if (source === "jobTitle") {
+      setTempJobInfo({ ...tempJobInfo, jobTitle: input });
+    } else if (source === "jobOverview") {
+      setTempJobInfo({ ...tempJobInfo, jobOverview: input });
+    }
+  }
+
+  // used for rendering the main work exp card
   function WorkExperienceDetails() {
     return (
       <div className="workExperienceDetails">
@@ -30,63 +75,27 @@ function WorkExpContainer() {
             dateT={state.dateUntil}
             jobT={state.jobTitle}
             jobDesc={state.jobOverview}
+            actionDelete={() => handleCardDelete(state.id)}
           />
         ))}
       </div>
     );
   }
 
-// resets state - used when closing modal by saving or cancelling addition  
-  function resetTempState() {
-    setTempJobInfo({
-    id: "", 
-    companyInput: "",
-    dateFrom: "",
-    dateUntil: "",
-    jobTitle: "",
-    jobOverview: ""
-    })
+  // card delete click handler
+  function handleCardDelete(id) {
+    const tempState = {...jobList};
+    const filteredState = Object.values(tempState).filter((job) => job.id != id);
+    setJobList(filteredState);
   }
-
-  function handleOpenClick() {
-    setTempJobInfo({id: uuidv4()})
-    setModalVisible(true);
-    console.log(tempJobInfo);
-    console.log(jobList)
-  }
-
-  function handleCloseClick() {
-    setModalVisible(false);
-    resetTempState();
-    console.log(tempJobInfo);
-  }
-
-// on clicking save, a new div should be created within workExp div called WorkExperienceDetails  
-  function handleSaveClick() {
-    setModalVisible(false);
-    setJobList([...jobList, tempJobInfo]);
-    console.log(jobList)
-    
-  }
-
-// need to create if / case for to handle data coming from different input sources
-function handleStateChange(input, source) {
-  if (source === "companyInput") {
-    setTempJobInfo({...tempJobInfo, companyInput: input})
-  } else if (source === "dateFrom") {
-    setTempJobInfo({...tempJobInfo, dateFrom: input})
-  } else if (source === "dateUntil") {
-    setTempJobInfo({...tempJobInfo, dateUntil: input})
-  } else if (source === "jobTitle") {
-    setTempJobInfo({...tempJobInfo, jobTitle: input})
-  } else if (source === "jobOverview") {
-    setTempJobInfo({...tempJobInfo, jobOverview: input})
-  } 
-}
 
   return (
     <div className="workExp">
-      <AddNewButton className="addWorkExp" text="+" action={handleOpenClick} />
+      <AddNewButton
+        className="addWorkExp"
+        text="Add job"
+        action={handleOpenClick}
+      />
       <WorkExperienceDetails />
       <WorkExpModal
         isOpen={modalVisible}
